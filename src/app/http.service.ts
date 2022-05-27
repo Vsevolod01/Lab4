c2782bf5df6faba84b89b3eeb4552fb3edab3430
Vsevolod01 <seva10052001@gmail.com>
2022-05-27 22:52:03 +0300

    Changed URLs for REST API

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -6,42 +6,47 @@ import {Dot} from "./main-page/main-page.component";
 
 @Injectable()
 export class HttpService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  regUrl: string = "http://localhost:8080/backend-1.0/api/register"
-  loginUrl: string = "http://localhost:8080/backend-1.0/api/login"
-  addDotUrl: string = "http://localhost:8080/backend-1.0/api/add"
-  removeDotsUrl: string = "http://localhost:8080/backend-1.0/api/clear"
-  dataUrl: string = "http://localhost:8080/backend-1.0/api/data"
+  regUrl: string = "http://localhost:8080/account/register"
+  loginUrl: string = "http://localhost:8080/account/login"
+  addDotUrl: string = "http://localhost:8080/dots"
+  removeDotsUrl: string = "http://localhost:8080/dots"
+  dataUrl: string = "http://localhost:8080/dots"
+
+  // regUrl: string = "http://localhost:8080/lab4-0.0.1-SNAPSHOT/account/register"
+  // loginUrl: string = "http://localhost:8080/lab4-0.0.1-SNAPSHOT/account/login"
+  // addDotUrl: string = "http://localhost:8080/lab4-0.0.1-SNAPSHOT/dots"
+  // removeDotsUrl: string = "http://localhost:8080/lab4-0.0.1-SNAPSHOT/dots"
+  // dataUrl: string = "http://localhost:8080/lab4-0.0.1-SNAPSHOT/dots"
 
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type':  'application/json',
+      'Content-Type': 'application/json',
     })
   };
 
-  regRequest(user: User) : Observable<any> {
+  regRequest(user: User): Observable<any> {
     let userJson: string = JSON.stringify(user)
     return this.http.post(this.regUrl, userJson)
   }
 
-  loginRequest(user: User) : Observable<any> {
+  loginRequest(user: User): Observable<any> {
     let userJson: string = JSON.stringify(user)
     return this.http.post(this.loginUrl, userJson)
   }
 
-  addRequest(dot: Dot, userToken: string) : Observable<any> {
+  addRequest(dot: Dot, userToken: string): Observable<any> {
     let dotJson: string = JSON.stringify(dot)
     return this.http.post(this.addDotUrl, dotJson, {headers: new HttpHeaders({"Authorization": userToken})})
   }
 
-  clearRequest(userToken: string) : Observable<any> {
+  clearRequest(userToken: string): Observable<any> {
     return this.http.delete(this.removeDotsUrl, {headers: new HttpHeaders({"Authorization": userToken})});
   }
 
-  dataRequest(userToken: string) : Observable<any> {
+  dataRequest(userToken: string): Observable<any> {
     return this.http.get(this.dataUrl, {headers: new HttpHeaders({"Authorization": userToken})});
   }
-
-
 }
